e76cbb7ae2c903247acd27659b9b90408254d2c5
Liam Reid <lreid295@gmail.com>
2024-01-01 23:05:58 +0000

    Adds elixir application

---
+++ lib/bulk_gcode_updater.ex
@@ -0,0 +1,18 @@
+defmodule BulkGcodeUpdater do
+  @moduledoc """
+  Documentation for `BulkGcodeUpdater`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> BulkGcodeUpdater.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/bulk_gcode_updater_test.exs
@@ -0,0 +1,8 @@
+defmodule BulkGcodeUpdaterTest do
+  use ExUnit.Case
+  doctest BulkGcodeUpdater
+
+  test "greets the world" do
+    assert BulkGcodeUpdater.hello() == :world
+  end
+end
